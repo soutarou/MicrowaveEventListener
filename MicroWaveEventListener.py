d7b8c69e0413bbfe67cef27e6b8a309cc9774b88
soutarou <kanikani.egg1210@gmail.com>
2018-11-21 02:53:38 +0900

    RMS窓を固定とし、チャンク単位で窓の中身を更新するように実装 ファイル出力(OUT)がTrue担っているときは、引数でファイルラベル名を要求するように実装

--- MicroWaveEventListener.py
+++ MicroWaveEventListener.py
@@ -5,58 +5,79 @@ import numpy as np
 import soundfile as sf
 import pyaudio as pa
 
-SOUND_INPUT_DEVICE = 2;
+import sys
+
+# 使うサウンドデバイス番号に合わせて変える
+# サウンドデバイス番号の確認はsoundDevice.pyで行う
+SOUND_INPUT_DEVICE = 0;
 CHUNK_SIZE = 1024
+RMS_BUFFSIZE = CHUNK_SIZE*4
+
+OUT = True
 
 # global
 xs = np.array([]) #サウンド出力用
-rms_buffSize = 0 #RMS窓のバッファ
-sum_power = 0 #RMS窓でのパワーの合計
+rmsDatas = np.array([]) #rms値のcsv出力用
+powerQueue = np.zeros(4) # chunkでの内積合計のキュー
+sum_rms_power = 0 #RMS窓での内積の合計
+
+def queue(data,input):
+    dst = np.roll(data,-1)
+    dst[-1] = input
+    return dst
 
 def callback(in_data, frame_count, time_info, status):
-    global xs
-    global rms_buffSize
-    global sum_power
+    global powerQueue
+    global sum_rms_power
 
     # short型のデータバッファをfloatに
     in_float = np.frombuffer(in_data, dtype=np.int16).astype(np.float)
-    # 値の範囲を-1.0 ~ 1,0に
+    # 値の範囲を-1.0 ~ 1,0に変換
     in_float[in_float > 0.0] /= float(2**15 - 1)
     in_float[in_float <= 0.0] /= float(2**15)
 
-    rms_buffSize += in_float.size
-
+    #chunk内の内積を計算
     power = in_float*in_float
-    sum_power += power.sum()
+    sum_power = power.sum()
 
-    # print("buffSize > ",rms_buffSize)
-    # print("power > ",sum_power)
-    rms = -1
-    if rms_buffSize >= CHUNK_SIZE*4:
-        rms = np.sqrt(sum_power/rms_buffSize)
-        print(rms)
-        sum_power = 0
-        rms_buffSize = 0
+    #RMS窓内の内積を計算
+    sum_rms_power += sum_power
+    sum_rms_power -= powerQueue[0]
 
-    # 入力音をサウンドファイルとして出力するならコメントアウトを外す
-    xs = np.r_[xs, in_float]
+    #chunkの内積をキューに追加し、先頭の要素を削除
+    powerQueue = queue(powerQueue,sum_power)
+
+    #RMS値を計算
+    rms = np.sqrt(sum_rms_power/RMS_BUFFSIZE)
+
+    if OUT:
+        global xs
+        global rmsDatas
+        xs = np.r_[xs, in_float]
+        rmsDatas = np.r_[rmsDatas, rms]
 
     return (in_data, pa.paContinue)
 
 if __name__ == "__main__":
-    # pyaudio
+
+    args = sys.argv
+
+    if OUT and len(args) < 2:
+            print("ラベル名を指定してください.")
+            print(" > python MicroWaveEventListener.py microwave")
+            exit()
+
     p_in = pa.PyAudio()
     py_format = p_in.get_format_from_width(2)
     fs = 16000
     channels = 1
-    chunk = CHUNK_SIZE
 
     # 入力ストリームを作成
     in_stream = p_in.open(format=py_format,
                           channels=channels,
                           rate=fs,
                           input=True,
-                          frames_per_buffer=chunk,
+                          frames_per_buffer=CHUNK_SIZE,
                           input_device_index=SOUND_INPUT_DEVICE,
                           stream_callback=callback)
 
@@ -73,7 +94,11 @@ if __name__ == "__main__":
         in_stream.stop_stream()
         in_stream.close()
 
-    # 入力信号を保存
-    sf.write("./pyaudio_output.wav", xs, fs)
+    if OUT:
+        # 入力信号を保存
+        wavPath = "./data/" + args[1] + "_sound.wav"
+        csvPath = "./data/" + args[1] + "_rms.csv"
+        sf.write(wavPath, xs, fs)
+        np.savetxt(csvPath, rmsDatas, delimiter=",")
 
     p_in.terminate()
