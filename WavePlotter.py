e656dc330cc511b50f666db04cbe2fe2fc621630
soutarou <kanikani.egg1210@gmail.com>
2018-12-05 15:10:24 +0900

    WavePlotter.pyを更新

--- WavePlotter.py
+++ WavePlotter.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+FS = 44100
+
 def main():
 
     args = sys.argv
@@ -13,8 +15,20 @@ def main():
     buf = wf.readframes(wf.getnframes())
     # バイナリデータを16bit整数に変換
     data = np.frombuffer(buf, dtype="int16")
-    np.savetxt("./data/wavfile.csv", data, delimiter=",")
-    plt.plot(data)
+
+    # short型のデータバッファをfloatに
+    in_float = np.frombuffer(data, dtype=np.int16).astype(np.float)
+    # 値の範囲を-1.0 ~ 1,0に変換
+    in_float[in_float > 0.0] /= float(2**15 - 1)
+    in_float[in_float <= 0.0] /= float(2**15)
+    
+    dt = 1/FS
+    t = np.arange(0, in_float.size*dt, dt) # 時間軸
+
+    plt.ylim([-0.25,0.25])
+    plt.plot(t,in_float)
+    plt.ylabel('Amplitude', fontsize=10)
+    plt.xlabel("Time[sec]", fontsize=10)
     plt.show()          # グラフ表示
 
 if __name__ == '__main__':
